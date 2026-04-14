a6c5227298ad506ff85534f27f324e65891f6611
Salavat Yagafarov <56019251+noisyfoar@users.noreply.github.com>
2026-04-14 16:01:08 +0500

    need to fix

--- View/ImportDialog.xaml.cs
+++ View/ImportDialog.xaml.cs
@@ -1,4 +1,4 @@
-using NPFGEO.ShellExtension.Formats.LIS.Dialogs.Import.ViewModels;
+using NPFGEO.ShellExtension.Formats.LIS.Dialogs.Import.ViewModel;
 using NPFGEO.ShellExtension.Formats.LIS.Dialogs.Import.Models;
 using System.Collections.Generic;
 using System.Windows;
@@ -8,29 +8,11 @@ namespace NPFGEO.ShellExtension.Formats.LIS.Dialogs.Import.View
 
     public partial class ImportDialog : Window
     {
-        private readonly ImportDialogViewModel _viewModel;
-
         public ImportDialog(IReadOnlyCollection<LISCurveItem> curves, IReadOnlyCollection<ParameterTable> parameterTables)
         {
             InitializeComponent();
-            _viewModel = new ImportDialogViewModel(curves, parameterTables);
-            DataContext = _viewModel;
-            _viewModel.RequestClose += OnRequestClose;
-            _viewModel.RequestCancel += OnRequestCancel;
         }
+        private ImportDialogViewModel _viewModel { get { return DataContext as ImportDialogViewModel; } }
 
-        public IReadOnlyList<LISCurveItem> SelectedCurvesResult => _viewModel.GetSelectedCurves();
-
-        private void OnRequestClose(object sender, System.EventArgs e)
-        {
-            DialogResult = true;
-            Close();
-        }
-
-        private void OnRequestCancel(object sender, System.EventArgs e)
-        {
-            DialogResult = false;
-            Close();
-        }
     }
 }
